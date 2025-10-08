19c328b67b73f1e4959791638cd0d9bafc8c63e0
uijin_lee <mr938363@gmail.com>
2025-10-08 15:07:15 +0900

    change title in server.py

--- server.py
+++ server.py
@@ -104,7 +104,7 @@ HTML_TEMPLATE = """
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>데이터 분석 실행</title>
+    <title>데이터 분석 실행을 합니다/title>
     <style>
         body {
             font-family: Arial, sans-serif;
